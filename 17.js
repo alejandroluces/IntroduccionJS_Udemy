c87d85d0b10f04ddd931056b4c9fd4424c025631
Alejandro Luces González <aluces@polytex.local>
2023-06-20 15:10:52 -0400

    Add course JavaScript training V17

---
+++ 17.js
@@ -0,0 +1,10 @@
+const numero1=20;
+const numero2= "20";
+
+// Diferencia entre funcion y metodo
+
+console.log(parseInt(numero2));//parseInt() es una funcion
+console.log(numero1.toString());// toString() es un metodo
+
+
+
