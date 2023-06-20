9bff83d3ce5d2d92fd86cb1247661603b2fa4289
Alejandro Luces González <aluces@polytex.local>
2023-06-20 15:20:37 -0400

    Add course JavaScript training V18

---
+++ 18.js
@@ -0,0 +1,19 @@
+// Declarade una funcion
+
+const sumar  = (numero1=0,numero2=0) =>{// numero1,numero2 son parametros
+    console.log(numero1+numero2);
+    
+}
+sumar(10,10);// son argumentos o valores reales
+sumar(10,20);
+sumar(1);
+
+
+// Ecpresion de la funcion
+
+const sumar2 =(n1,n2)=> {
+
+    console.log(n1+n2);
+
+}
+sumar2(10,18);
