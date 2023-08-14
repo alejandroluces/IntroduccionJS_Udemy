214135507940709a82f6525034202795f9555cdd
Alejandro Luces González <aluces@polytex.local>
2023-08-14 16:30:29 -0400

    Modf_course_JavaScript_training_V27

--- js/27_objConstr_objLiteral.js
+++ js/27_objConstr_objLiteral.js
@@ -7,12 +7,19 @@ const producto={
 }
 
 //Object Constructor
+function Cliente(nombre,apellido){
+    this.nombre=nombre
+    this.apellido=apellido
+}
 
-function Producto(nombre,precio, diponibilidad){
+Cliente.prototype.formatearCliente=function(){
+    return `El producto ${this.nombre} tiene un precio de: ${this.apellido}`
+}
+
+function Producto(nombre,precio){
     this.nombre=nombre
     this.precio=precio
-    this.diponibilidad=diponibilidad
-
+    
 }
 
 const producto2=new Producto("Monitor Curvo de 49",800,true)
@@ -20,8 +27,27 @@ const producto3=new Producto("Notebook",300,false)
 const producto4=new Producto("Teclado",100,false)
 const producto5=new Producto("Tablet",300,true)
 
+const formatearProducto=(producto)=>{
+    return `El producto ${producto.nombre} tiene un precio de: ${producto.precio}`
+}
+
+console.log(formatearProducto(producto2));
+const cliente=new Cliente("Juan","De la torre")
+
+console.log(cliente);
+
+
 
 console.log(producto2);
 console.log(producto3);
-console.log(producto4);
-console.log(producto5);
+
+// Crear funciones que solo se utilizan en un objeto en especifico
+Producto.prototype.formatearProducto=function(producto){
+    return `El producto ${this.nombre} tiene un precio de: ${this.precio}`
+}
+
+console.log(producto2)
+
+console.log(producto2.formatearProducto());
+console.log(producto3.formatearProducto());
+
